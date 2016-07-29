a2e7d3d99c263abf40e38ecb6a3df9d7018715c1
Esun <esun@EsundeMacBook-Pro.local>
2016-07-29 09:26:40 +0800

    add python_access and a script

---
+++ python_access/random_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2.7
+#-*- coding:utf-8 -*-
+#用于生成随机mac地址
+
+import random
+
+grandmac = ""
+randmac_txt = file('randmac.txt', 'w')
+
+for n in range(1, 10):
+    x = []
+    for i in range(1, 7):
+        randnum = ''.join(random.sample('0123456789abcdef', 2))
+        x.append(str(randnum))
+    randmac = ":".join(x)
+    grandmac += randmac
+    grandmac += "\n"
+print >> randmac_txt, grandmac
